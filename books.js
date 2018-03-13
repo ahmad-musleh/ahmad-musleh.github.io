c3e93b572fc418ed53d22c6091f006f2aa938a96
ahmad-musleh <musl3h@Ahmads-MBP.fios-router.home>
2018-03-13 13:58:58 -0400

    updating personal data

--- books.js
+++ books.js
@@ -11,10 +11,9 @@
 
 var books = [
         ["Title", "Author(s)"],
-        ["The Top Gear Years", "Jeremy Clarkson"],
+        ["Rich Dad Poor Dad", "Robert Kiyosaki"],
+        ["Outliers: The Story of Success", "Malcom Gladwell"],
         ["Judgment In Managerial Decision Making", "Max Bazerman and Don Moore"],
         ["Spam Nation: The inside story of organized cyber crime from global epidemic to your front door" , "Brian Krebs"],
-        ["Animal Farm" , "George Orwell"],
-        ["1984" , "George Orwell"],
         ["The leadership Myth" , "Martin B. Friedman"]
 ]
